8439d081788baf6014d05b1d4b707576d0639a3a
Keller Jordan <kellerjordan1998@gmail.com>
2024-11-19 19:57:40 -0800

    revert remove too much gettext

--- notebook/notebookapp.py
+++ notebook/notebookapp.py
@@ -212,6 +212,9 @@ class NotebookWebApplication(web.Application):
         base_dir = os.path.realpath(os.path.join(__file__, '..', '..'))
         dev_mode = os.path.exists(os.path.join(base_dir, '.git'))
 
+        nbui = gettext.translation('nbui', localedir=os.path.join(base_dir, 'notebook/i18n'), fallback=True)
+        env.install_gettext_translations(nbui, newstyle=False)
+
         if dev_mode:
             DEV_NOTE_NPM = f"""It looks like you're running the notebook from source.
     If you're working on the Javascript of the notebook, try running
